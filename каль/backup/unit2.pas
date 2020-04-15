06e291cf5fc39862c1aa58df72bf9a3cf920c5f6
kopotilo <62011819+kopotilo@users.noreply.github.com>
2020-04-15 03:38:35 +0300

    Add files via upload
    v1.0 Базовый и начало расширенного

---
+++ каль/backup/unit2.pas
@@ -0,0 +1,161 @@
+unit Unit2;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls;
+
+type
+
+  { TForm2 }
+
+  TForm2 = class(TForm)
+    Button1: TButton;
+    Button10: TButton;
+    Button11: TButton;
+    Button12: TButton;
+    Button13: TButton;
+    Button14: TButton;
+    Button15: TButton;
+    Button16: TButton;
+    Button17: TButton;
+    Button18: TButton;
+    Button19: TButton;
+    Button2: TButton;
+    Button20: TButton;
+    Button21: TButton;
+    Button22: TButton;
+    Button23: TButton;
+    Button24: TButton;
+    Button25: TButton;
+    Button26: TButton;
+    Button27: TButton;
+    Button28: TButton;
+    Button3: TButton;
+    Button4: TButton;
+    Button5: TButton;
+    Button6: TButton;
+    Button7: TButton;
+    Button8: TButton;
+    Button9: TButton;
+    Edit2: TEdit;
+    procedure Button16Click(Sender: TObject);
+    procedure Button17Click(Sender: TObject);
+    procedure Button18Click(Sender: TObject);
+    procedure Button20Click(Sender: TObject);
+    procedure Button21Click(Sender: TObject);
+    procedure Button22Click(Sender: TObject);
+    procedure Button24Click(Sender: TObject);
+    procedure Button25Click(Sender: TObject);
+    procedure Button26Click(Sender: TObject);
+    procedure ClickButt(Sender: TObject);
+    procedure ClickZnakk(Sender: TObject);
+
+  private
+
+  public
+
+  end;
+
+var
+  Form2: TForm2;
+  a, b, c : Real; //Для хранения промежуточных значений
+    znak : String;
+implementation
+    uses Unit1, math;
+
+{$R *.lfm}
+
+{ TForm2 }
+procedure TForm2.ClickButt(Sender: TObject);  // Для кнопок от 0 до 9
+    begin
+       Edit2.Text:=Edit2.Text + ( Sender as TButton).Caption;
+end;
+
+procedure TForm2.Button22Click(Sender: TObject);  // posl simvol
+var
+   str: String;
+begin
+   str := Edit2.Text;
+   if str <> '' then
+   Delete(str, Length(str),1);
+   edit2.Text:= str;
+end;
+
+procedure TForm2.Button24Click(Sender: TObject);  // sin
+begin
+  a := StrToInt(Edit2.Text); // Здесь - угол в градусах
+  Edit2.Text := FloatToStr(sin(degtorad(a))); // Здесь - значение синуса этого угла
+end;
+
+procedure TForm2.Button25Click(Sender: TObject); // cos
+begin
+ a := StrToInt(Edit2.Text); // Здесь - угол в градусах
+  Edit2.Text := FloatToStr(cos(degtorad(a))); // Здесь - значение синуса этого угла
+end;
+
+procedure TForm2.Button26Click(Sender: TObject);
+begin
+  a := StrToFloat(Edit2.Text);
+  a := sqr(b);
+  Edit2.Text:=FloatToStr(b);
+
+end;
+
+procedure TForm2.Button21Click(Sender: TObject);  // vse simvoli ydalenie
+begin
+  Edit2.Clear;
+end;
+
+procedure TForm2.Button20Click(Sender: TObject);  // a i b ydalenie
+begin
+  Edit2.Clear;
+  a:=0;
+  b:=0;
+  c:=0;
+end;
+
+procedure TForm2.Button16Click(Sender: TObject);  // =
+begin
+   b := strToFloat(Edit2.Text);
+  Edit2.Clear;
+  case znak of
+  '+' : c := a+b;
+  '-' : c := a-b;
+  '*' : c := a*b;
+  '/' : c := a/b;
+
+ end;
+  Edit2.Text:= FloatToStr(c);
+end;
+
+procedure TForm2.Button17Click(Sender: TObject); //koren'
+begin
+  a := StrToFloat(Edit2.Text);
+  a := sqrt(a);
+  Edit2.Text:=FloatToStr(a);
+  a := 0;
+end;
+
+procedure TForm2.Button18Click(Sender: TObject); // x^2
+begin
+   a := StrToFloat(Edit2.Text);
+  a := sqr(a);
+  Edit2.Text:=FloatToStr(a);
+  a := 0;
+end;
+
+procedure TForm2.ClickZnakk(Sender: TObject);
+begin
+  a := StrToFloat(Edit2.Text);
+    Edit2.Clear;
+
+    znak :=(Sender as TButton).Caption;
+end;
+
+
+
+end.
+
---
+++ каль/unit1.pas
@@ -0,0 +1,173 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, Menus;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Button10: TButton;
+    Button11: TButton;
+    Button12: TButton;
+    Button13: TButton;
+    Button14: TButton;
+    Button15: TButton;
+    Button16: TButton;
+    Button17: TButton;
+    Button18: TButton;
+    Button19: TButton;
+    Button2: TButton;
+    Button20: TButton;
+    Button21: TButton;
+    Button22: TButton;
+    Button23: TButton;
+    Button3: TButton;
+    Button4: TButton;
+    Button5: TButton;
+    Button6: TButton;
+    Button7: TButton;
+    Button8: TButton;
+    Button9: TButton;
+    Edit1: TEdit;
+    MainMenu1: TMainMenu;
+    MenuItem1: TMenuItem;
+    MenuItem2: TMenuItem;
+    MenuItem3: TMenuItem;
+    MenuItem4: TMenuItem;
+    MenuItem5: TMenuItem;
+    MenuItem6: TMenuItem;
+    MenuItem7: TMenuItem;
+    MenuItem8: TMenuItem;
+    MenuItem9: TMenuItem;
+    procedure Button16Click(Sender: TObject);
+    procedure Button17Click(Sender: TObject);
+    procedure Button18Click(Sender: TObject);
+
+    procedure Button20Click(Sender: TObject);
+    procedure Button21Click(Sender: TObject);
+    procedure Button22Click(Sender: TObject);
+    procedure Button23Click(Sender: TObject);
+    procedure ClickBut(Sender: TObject);
+    procedure ClickZnak(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure MenuItem8Click(Sender: TObject);
+
+
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+  a, b, c : Real; //Для хранения промежуточных значений
+  znak : String;
+implementation
+   Uses Unit2; // юзяем формы
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.ClickBut(Sender: TObject);  // Для кнопок от 0 до 9
+begin
+   Edit1.Text:=Edit1.Text + ( Sender as TButton).Caption;
+end;
+
+procedure TForm1.Button22Click(Sender: TObject);  // Удаление последнего символа
+var
+   str: String;
+begin
+   str := Edit1.Text;
+   if str <> '' then
+   Delete(str, Length(str),1);
+   edit1.Text:= str;
+end;
+
+procedure TForm1.Button23Click(Sender: TObject); // Измененине знака
+begin
+  if Edit1.Text <> '0' then
+  begin
+    if Edit1.Text[1] = '-' then Edit1.Text := RightStr(Edit1.Text, Length(Edit1.Text) - 1)
+    else Edit1.Text := '-' + Edit1.Text;
+  end;
+end;
+
+procedure TForm1.Button21Click(Sender: TObject);  // Удаление всех символов
+begin
+  Edit1.Clear;
+end;
+
+procedure TForm1.Button16Click(Sender: TObject); // равно
+begin
+  b := strToFloat(Edit1.Text);
+  Edit1.Clear;
+  case znak of
+  '+' : c := a+b;
+  '-' : c := a-b;
+  '*' : c := a*b;
+  '/' : c := a/b;
+
+ end;
+  Edit1.Text:= FloatToStr(c);
+end;
+
+procedure TForm1.Button17Click(Sender: TObject);  // Корень из числа
+begin
+  a := StrToFloat(Edit1.Text);
+  a := sqrt(a);
+  Edit1.Text:=FloatToStr(a);
+  a := 0;
+end;
+
+procedure TForm1.Button18Click(Sender: TObject); // Квадрат числа
+begin
+  a := StrToFloat(Edit1.Text);
+  a := sqr(a);
+  Edit1.Text:=FloatToStr(a);
+  a := 0;
+end;
+
+
+
+procedure TForm1.Button20Click(Sender: TObject); // ydalenie a i b
+begin
+  Edit1.Clear;
+  a:=0;
+  b:=0;
+  c:=0;
+end;
+
+procedure TForm1.ClickZnak(Sender: TObject);  //
+begin
+  a := StrToFloat(Edit1.Text);
+  Edit1.Clear;
+
+  znak :=(Sender as TButton).Caption;
+
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.MenuItem8Click(Sender: TObject); // расширенный калькулятор
+begin
+  Unit2.form2.show;
+  Unit1.form1.Hide;
+end;
+
+
+
+
+
+end.
+
